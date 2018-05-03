3420d2e526fe280c0c85da10312941fc26c8c976
mattlindvig <mattlindvig@gmail.com>
2018-05-03 10:30:13 -0500

    rest of it

---
+++ Quotations.UI/Quotations.UI/Models/Quote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quotations.UI.Models
+{
+    public class Quote
+    {
+        public int QuoteId { get; set; }
+        public Author Author { get; set; }
+        public string Quotation { get; set; }
+        public User Users { get; set; }
+        public DateTimeOffset CreatedDateTime { get; set; }
+        public string SourceInfo { get; set; }
+
+    }
+}
