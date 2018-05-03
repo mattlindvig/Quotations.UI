b8694dc35a07835eaf245c2990d7022d7e619c62
mattlindvig <mattlindvig@gmail.com>
2018-05-03 14:34:45 -0500

    Basic view to try to create a quote
    I'm about to try something and I don't want to screw anything up, thus checking in whatever I have.

--- Quotations.UI/Quotations.UI/Controllers/HomeController.cs
+++ Quotations.UI/Quotations.UI/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace Quotations.UI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult CreateQuote()
+        {
+            return View();
+        }
     }
 }
---
+++ Quotations.UI/Quotations.UI/Controllers/QuotesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quotations.UI.Controllers
+{
+    public class QuotesController : Controller
+    {
+        public IActionResult CreateQuote()
+        {
+            return View();
+        }
+    }
+}
